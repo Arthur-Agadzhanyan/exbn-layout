7bf51c8cc19ca206d8c36ae67013dae6f6976dca
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-05-17 00:16:40 +0300

    adapted: to 1440px

--- dist/js/script.js
+++ dist/js/script.js
@@ -33,14 +33,36 @@ const mainPagePartnersSlider = new Swiper('#main_page-partners_slider', {
     slidesPerView: 5.7,
     speed: 1000,
     spaceBetween: 16,
-    loop:true
+    loop:true,
+    breakpoints:{
+        1100:{
+            slidesPerView: 4.3,
+        },
+        1500:{
+            slidesPerView: 4.7,
+        },
+        1750:{
+            slidesPerView: 5.7,
+        }
+    }
 });
 
 const mainPageNewsSlider = new Swiper('#main_page-news_slider', {
-    slidesPerView: 3.8,
+    
     speed: 1000,
     spaceBetween: 16,
-    loop:true
+    loop:true,
+    breakpoints:{
+        1100:{
+            slidesPerView: 2.8,
+        },
+        1500:{
+            slidesPerView: 3.3,
+        },
+        1750:{
+            slidesPerView: 3.8,
+        }
+    }
 });
 
 AOS.init()
