526e12e1f2d832cb40733a9c70940de7f688ecf2
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-05-16 21:05:04 +0300

    added: main page, not adapted

---
+++ src/js/pages/main_page.js
@@ -0,0 +1,13 @@
+const mainPagePartnersSlider = new Swiper('#main_page-partners_slider', {
+    slidesPerView: 5.7,
+    speed: 1000,
+    spaceBetween: 16,
+    loop:true
+});
+
+const mainPageNewsSlider = new Swiper('#main_page-news_slider', {
+    slidesPerView: 3.8,
+    speed: 1000,
+    spaceBetween: 16,
+    loop:true
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 @@include('webpTest.js');
 @@include('swiper.js');
+@@include('pages/main_page.js')
 
 AOS.init()
-
 
 const navbarBtn = document.getElementById('navbar_btn')
 const navbarPoppup = document.getElementById('navbar_poppup')
