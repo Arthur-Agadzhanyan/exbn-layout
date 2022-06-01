b807acb816130138d3d9817793d31428d6f91c65
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-06-01 20:25:40 +0300

    minor changes

--- src/js/pages/about_page.js
+++ src/js/pages/about_page.js
@@ -2,7 +2,6 @@ const aboutPagePartnersSlider = new Swiper('#about_page-partners_slider', {
     slidesPerView: 5.7,
     speed: 1000,
     spaceBetween: 16,
-    loop:true,
     breakpoints:{
         1100:{
             slidesPerView: 4.3,
